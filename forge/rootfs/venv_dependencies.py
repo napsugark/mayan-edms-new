f6caa5b6e4c078e3e5b076a2520100bf37fc368f
Roberto Rosario <roberto.rosario@mayan-edms.com>
2025-08-24 09:52:07 +0000

    Code style cleanup
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- forge/rootfs/venv_dependencies.py
+++ forge/rootfs/venv_dependencies.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-import os, subprocess, sys
+import os
+import subprocess
+import sys
 
 dependency_string = os.environ.get('FORGE_PYTHON_PACKAGES', '')
 dependency_list = dependency_string.split(' ')
